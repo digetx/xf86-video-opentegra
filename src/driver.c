wzyy2/xf86-video-modesetting

Use own thunk function instead of shadowUpdatePackedWeak
I plan to remove the Weak functions from a future server.

Signed-off-by: Adam Jackson <ajax@redhat.com>

src/driver.c
@@ -810,6 +810,12 @@ msShadowWindow(ScreenPtr screen, CARD32 row, CARD32 offset, int mode,
     return ((uint8_t *)ms->drmmode.front_bo->ptr + row * stride + offset);
 }
 
+static void
+msUpdatePacked(ScreenPtr pScreen, shadowBufPtr pBuf)
+{
+    shadowUpdatePacked(pScreen, pBuf);
+}
+
 static Bool
 CreateScreenResources(ScreenPtr pScreen)
 {
@@ -842,7 +848,7 @@ CreateScreenResources(ScreenPtr pScreen)
 	FatalError("Couldn't adjust screen pixmap\n");
 
     if (ms->drmmode.shadow_enable) {
-	if (!shadowAdd(pScreen, rootPixmap, shadowUpdatePackedWeak(),
+	if (!shadowAdd(pScreen, rootPixmap, msUpdatePacked,
 		       msShadowWindow, 0, 0))
 	    return FALSE;
     }

digetx/xf86-video-opentegra

src/driver.c
@@ -526,6 +526,12 @@ TegraShadowWindow(ScreenPtr screen, CARD32 row, CARD32 offset, int mode,
     return ((uint8_t *)tegra->drmmode.front_bo->ptr + row * stride + offset);
 }
 
+static void
+TegraUpdatePacked(ScreenPtr pScreen, shadowBufPtr pBuf)
+{
+    shadowUpdatePacked(pScreen, pBuf);
+}
+
 static Bool
 TegraCreateScreenResources(ScreenPtr pScreen)
 {
@@ -560,7 +566,7 @@ TegraCreateScreenResources(ScreenPtr pScreen)
         FatalError("Couldn't adjust screen pixmap\n");
 
     if (tegra->drmmode.shadow_enable) {
-        if (!shadowAdd(pScreen, rootPixmap, shadowUpdatePackedWeak(),
+        if (!shadowAdd(pScreen, rootPixmap, TegraUpdatePacked,
                        TegraShadowWindow, 0, 0))
             return FALSE;
     }
@@ -934,4 +940,5 @@ static XF86ModuleVersionInfo VersRec = {
 _X_EXPORT XF86ModuleData opentegraModuleData = { &VersRec, Setup, NULL };
 
 /* vim: set et sts=4 sw=4 ts=4: */
+
 